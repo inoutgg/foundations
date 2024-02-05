d19111c45e3428a867ddf4b7ab5b266c7252c8fb
Roman Vanesyan <roman@vanesyan.com>
2024-02-05 17:04:26 +0200

    chore: rename chain to applicator group

--- http/util/router/applicator.go
+++ http/util/router/applicator.go
@@ -16,17 +16,17 @@ type ApplicatorFunc func(chi.Router) chi.Router
 
 func (af ApplicatorFunc) Apply(r chi.Router) chi.Router { return af(r) }
 
-type Chain struct {
+type ApplicatorGroup struct {
 	applicators []Applicator
 }
 
 // NewChain creates a new chain of router applicators.
-func NewChain(applicators ...Applicator) Chain {
-	return Chain{applicators}
+func NewChain(applicators ...Applicator) ApplicatorGroup {
+	return ApplicatorGroup{applicators}
 }
 
 // Apply applies applicators to a given router.
-func (c Chain) Apply(r chi.Router) chi.Router {
+func (c ApplicatorGroup) Apply(r chi.Router) chi.Router {
 	for _, applicator := range c.applicators {
 		r = applicator.Apply(r)
 	}
