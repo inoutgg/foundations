4438876d0dca144796fe0c331f28bac163c50b08
Roman Vanesyan <roman@vanesyan.com>
2025-07-21 00:59:36 +0300

    chore: use logger

--- http/httplog/middleware.go
+++ http/httplog/middleware.go
@@ -8,12 +8,13 @@ import (
 	"go.inout.gg/foundations/http/httpmiddleware"
 )
 
-func Middleware(log *slog.Logger) httpmiddleware.MiddlewareFunc {
+// Middleware logs incoming requests with the provided logger l.
+func Middleware(l *slog.Logger) httpmiddleware.MiddlewareFunc {
 	return httpmiddleware.MiddlewareFunc(func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			ctx := r.Context()
 			metrics := httpsnoop.CaptureMetrics(next, w, r)
-			slog.InfoContext(
+			l.InfoContext(
 				ctx,
 				"incoming request",
 				slog.String("method", r.Method),
